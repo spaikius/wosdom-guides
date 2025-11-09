f42992af5628d09ff50e842272daf94cbe5456a7
spaikius <rimvydas.nore@gmail.com>
2025-11-10 01:18:15 +0200

    fix: duplicate nested guides structure

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
       targets: [
         {
           src: "guides/**/*.{png,jpg,jpeg,gif,webp}",
-          dest: "guides",
+          dest: "",
         },
       ],
     }),
