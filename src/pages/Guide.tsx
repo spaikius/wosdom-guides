93b29f578add0fc859877516d5644bf4af62f689
spaikius <rimvydas.nore@gmail.com>
2025-11-10 00:57:32 +0200

    feat: add back to guides button

--- src/pages/Guide.tsx
+++ src/pages/Guide.tsx
@@ -1,7 +1,9 @@
-import { useParams } from '@tanstack/react-router';
+import { useParams, Link } from '@tanstack/react-router';
 import { PageContainer } from '@/components/layout/PageContainer';
 import { ROUTES } from '@/config/routes';
 import { useGuide } from '@/hooks/useGuide';
+import { Button } from '@/components/ui/button';
+import { BookOpenText } from 'lucide-react';
 
 export const Guide: React.FC = () => {
   const { slug } = useParams({ from: ROUTES.GUIDE });
@@ -13,6 +15,15 @@ export const Guide: React.FC = () => {
         {/* biome-ignore lint/security/noDangerouslySetInnerHtml: HTML is sanitized with DOMPurify */}
         <div dangerouslySetInnerHTML={{ __html: html }} />
       </article>
+
+      <div className="pt-6 flex justify-center">
+        <Button asChild size="lg" className="gap-2">
+          <Link to={ROUTES.GUIDES}>
+            <BookOpenText className="h-4 w-4" />
+            Back to Guides
+          </Link>
+        </Button>
+      </div>
     </PageContainer>
   );
 };
