2c8626d242be1543e605913df4aee504b32728c1
spaikius <spaikius@users.noreply.github.com>
2025-11-09 23:44:46 +0000

    deploy: 71f8b0dd8ae4c82e4ba8100815afbb1bc0f90bdb

---
+++ assets/guide-DsGszAnm.js
@@ -0,0 +1,16 @@
+const e=`# 🥚 **Ahaha! You Found an Easter Egg!**
+
+🎉 Congratulations, brave explorer — you've uncovered one of WOSdom's _hidden_ secrets!
+
+Not much to see here… or is there? 👀
+
+You were probably poking around where no one told you to,  
+and for that, we salute you. 🫡
+
+> _"In chaos, we find discovery."_ — Probably Spaikius, after breaking something
+
+Enjoy your victory, and remember:  
+the **real treasure** was the curiosity we broke along the way. 💙
+
+![Easter egg](./easter-egg.png)
+`;export{e as default};
