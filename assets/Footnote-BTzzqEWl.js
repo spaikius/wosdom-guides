78aa9bc83c51992346e200414520f9fc1f3d0311
spaikius <spaikius@users.noreply.github.com>
2025-12-24 20:44:49 +0000

    deploy: 2fb4f22e429afa266b30068e246410c41e9da933

--- assets/Footnote-BTzzqEWl.js
+++
@@ -1,1 +0,0 @@
-import{j as e,b as r}from"./index-B-J2_byb.js";const n=({children:t,className:o,...s})=>e.jsx("p",{className:r("text-xs text-muted-foreground",o),...s,children:t});export{n as F};
---
+++ assets/Footnote-Dhbj_88d.js
@@ -0,0 +1,1 @@
+import{j as e,b as r}from"./index-BAZNUhB7.js";const n=({children:t,className:o,...s})=>e.jsx("p",{className:r("text-xs text-muted-foreground",o),...s,children:t});export{n as F};
