62ff48f243426226c99f858ee7782da6b0b9653b
spaikius <spaikius@users.noreply.github.com>
2026-01-01 20:09:39 +0000

    deploy: 0ab1bc7bb7e882f8376575f1c4d1c907badc5866

---
+++ assets/guide-e1sBJ6NB.js
@@ -0,0 +1,96 @@
+const n=`# Rally Assignments
+
+## 🚩 Rally Leaders / Starters
+
+- **[MiS] Zantas**
+- **[HKM] Paros**
+- **[HKM] MASTERofPUPPETS**
+- **[HKM] Kangaroo~~~鼠**
+- **[HKM] #STAR**
+- **[HKM] Dr Evil Minion**
+- **[HKM] yourmoms**
+
+# Assignments
+
+## **[MiS] Zantas** (You will need to use 20% Deployment Capacity boost II)
+
+- 1 [MiS]Frank 龍
+- 2 [MiS]DMT
+- 3 [MiS]spaikius
+- 4 [MiS]91 神秘
+- 5 [MiS]Ice Queen
+- 6 [MiS]KAMIKAZE
+- 7 [MiS]AriaD
+
+> People assigned to Zantas rallies, please read the important notice at the bottom of the page!
+
+## **[HKM] Paros** (You will need to use 20% Deployment Capacity boost II)
+
+- 1 [HKM]Checkit (From [VLF])
+- 2 [HKM]zhi (From [VLF])
+- 3 [HKM]SAJA Toss (From [ROK])
+- 4 [HKM]-Freedom~愛自由 (From [MiS])
+- 5 [HKM]Dwayne93 (From [MiS])
+- 6 [HKM]Kuba B12 (From [MiS])
+- 7 [HKM]freeze (From [MiS])
+
+## **[HKM] MASTERofPUPPETS**
+
+- 1 [HKM]Leo1995 (from [HxH])
+- 2 [HKM]CaitMawr (from [HxH])
+- 3 [HKM]SHAHIN & JASSAR (from [HxH])
+- 4 [HKM]Qin Shi Huang (from [HxH])
+- 5 [HKM]geosicleber 252 (from [ROK])
+- 6 [HKM]Jorenji Quinn (from [VLF])
+- 7 [HKM]Aga (from [VLF])
+- 8 [HKM]yourmoms (from [HxH])
+
+## **[HKM] Kangaroo~~~鼠**
+
+- 1 [HKM]Capy Farm (from [VLF])
+- 2 [HKM]Black Cat ツ (from [MiS])
+- 3 [HKM]Belmund0 (from [MiS])
+- 4 [HKM]RAJU (from [MiS])
+- 5 [HKM]Tomato 番茄 (from [MiS])
+- 6 [HKM]Witcher (from [MiS])
+- 7 [HKM]ʕ·ᴥ·ʔK킼킼킼ᴷᴵᴷ (from [ROK])
+
+## **[HKM] #STAR**
+
+- 1 [HKM]Shironeko (from [MiS])
+- 2 [HKM]ESREF TEK (from [ROK])
+- 3 [HKM]QueenScobar (from [VLF])
+- 4 [HKM]Josike (from [VLF])
+- 5 [HKM]Lucario (from [HxH])
+- 6 [HKM]TIRTHO (from [HxH])
+- 7 [HKM]Birger (from [HxH])
+- 8 [HKM]ahmed9920 (from [HxH])
+
+## **[HKM] Dr Evil Minion**
+
+- 1 [HKM]Krutek (from [HxH])
+- 2 [HKM]WaterWorld (from [HxH])
+- 3 [HKM]Honor (from [HxH])
+- 4 [HKM]Shadow Strike (from [HxH])
+- 5 [HKM]ice cream (from [HxH])
+- 6 [HKM]mahmoud (from [HxH])
+- 7 [HKM]Woy02 (from [HxH])
+- 8 [HKM]Virus (from [HxH])
+
+## Free of duty
+
+- [HxH]Zestrax
+- [HxH]Greybeard
+
+## ⚠️ IMPORTANT NOTICE for Zantas Rally Joiners!
+
+If Zantas starts a rally to retake SFC, you **must join with OFFENSIVE formation** ⚔️  
+This is **bad for defense**, so after capture:
+
+### 🧩 What You Must Do
+
+1️⃣ Send **new defensive reinforcements** to the castle  
+2️⃣ Wait until they are **~5 seconds away**  
+3️⃣ **Relief / cancel** your current troops  
+➡️ New defensive troops replace them instantly
+`;export{n as default};
