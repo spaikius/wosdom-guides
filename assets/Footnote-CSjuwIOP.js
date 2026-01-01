412c305d6cc668b8224c8f8828ccf32a3af00b81
spaikius <spaikius@users.noreply.github.com>
2026-01-01 18:36:32 +0000

    deploy: 1e33ba6efb737ced063f2054975446be3c824667

---
+++ assets/Footnote-CSjuwIOP.js
@@ -0,0 +1,1 @@
+import{j as s,e as r}from"./index-3HAoGK5y.js";const n=({children:t,className:e,...o})=>s.jsx("p",{className:r("text-xs text-muted-foreground",e),...o,children:t});export{n as F};
---
+++ assets/PageContainer-cIzZasSL.js
@@ -0,0 +1,1 @@
+import{j as m,e as o}from"./index-3HAoGK5y.js";const r=({children:s,className:a,maxWidth:e="max-w-3xl",...t})=>m.jsx("section",{className:o("w-full mx-auto space-y-8 md:space-y-10 md:pt-6",e,a),...t,children:s});export{r as P};
